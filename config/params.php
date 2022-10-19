3be1eeb05d802c5d96136a8caf42cbba71b52dcf
vladyka-vadim <vladyka.vadim@mail.ru>
2022-10-19 16:03:43 +0300

    fix project files

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'admin_email' => 'vladyka.vadim@yandex.ru',
+    'shop_name' => 'Магазин ishop2.loc',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.yandex.ru',
+    'smtp_port' => '465',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'vladyka.vadim@yandex.ru',
+    'smtp_password' => 'hrsalnictqxzogxc',
+];
